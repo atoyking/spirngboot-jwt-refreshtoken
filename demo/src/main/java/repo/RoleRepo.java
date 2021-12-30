c0d60efebd35b861aa510ad229b3d86a580208ca
atoyking12 <jhaards@gmail.com>
2021-12-31 04:47:06 +0800

    made a springboot jwt token and refresh token

---
+++ demo/src/main/java/repo/RoleRepo.java
@@ -0,0 +1,9 @@
+package repo;
+
+import domain.Role;
+import domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
